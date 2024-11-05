108ff465e843c707af0c7d9b4bd660526d0dd468
Kr1pto-psix <agent.facit.06@bk.ru>
2024-11-06 01:12:22 +0300

    home task 2 working

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
 
 
+def betterSum(lst):
+    total_sum = sum(x for x in lst if isinstance(x, (int, float)))  # Сумма всех чисел
+    concatenated_str = ''.join(x for x in lst if isinstance(x, str))  # Конкатенация всех строк
+    if total_sum == 0:
+        return concatenated_str
+    return str(total_sum)
 
-def task1():
-    l = []
-    for i in range(5):
-        num = int(input("Enter the number: "))
-        l.append(num)
+def task2(a):
+    return all(int(c) % 2 == 0 for c in str(a))
 
-    count = 0
-    for i in l:
-        if i >= 10 and i <= 100:
-                  count += 1
-    return count              
+def task3(a):
+   return all(int(c) < 5 for c in str(a))
 
-def task2():
-    count = 0
-    while count < 100:
-        num = int(input("Enter the num: "))
-        if num % 10 == 7:
-            count += num
+def task4(s):
+   return any(c in 'aeiouAEIOU' for c in s)
 
-def task3():
-    l = []
-    end = False
-    while True:
-        num = int(input("Enter the num: "))
-        l.append(num)
-        if num == 0:
-            if end == True:
-                break
-            end = True
-    return l        
+def is_prime(n):
+    return n > 1 and all(n % i != 0 for i in range(2, int(n**0.5) + 1))
+
 
 
 if __name__ == "__main__":
-    print("Program started")
-    print("begin task1")
-    print(f"task1: {task1()}")   
-    print("task2 begin")
-    task2()
-    print("task2 end")
-    print("task 3 begin")
-    print(f"task3: {task3()}")
-    
+    l = [1, 2, 3, 4]
+    s = ["qwe", "asd", "qwe"]
+    print(betterSum(l))  # 10
+    print(betterSum(s))  # qweasdqwe
+    a = 1348
+    print(task2(a))  # False
+    a = 2468
+    print(task2(a)) # true
 
+    a = 131231
+    print(task3(a))# True
+    a = 1234
+    print(task3(a)) # False
+    s = "Hello World"
+    print(task4(s))# True
+    s = "Hll Wrld"
+    print(task4(s)) # False
+
+    print(is_prime(7))  # True
+    print(is_prime(10))  # False
+    input()
 
